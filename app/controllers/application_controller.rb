62937152f31ee87ba8488da858164642595686cc
serjpetrenko <b.g.lupus@gmail.com>
2017-03-06 01:25:02 +0200

    Added actions to menu controller, added render_404 func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  
+  def render_403
+    render file: "public/403.html", status: 403
+  end
+
+  def render_404
+    render file: "public/404.html", status: 404
+  end
+
 end
