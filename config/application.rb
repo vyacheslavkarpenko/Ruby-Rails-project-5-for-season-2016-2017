00845be0e23d3735bda1b8e42fc9b8cf05fde8b1
serjpetrenko <b.g.lupus@gmail.com>
2017-03-21 01:44:46 +0200

    Commented some code

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,15 @@ Bundler.require(*Rails.groups)
 module RubyRailsProject5ForSeason20162017
   class Application < Rails::Application
 
-    config generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
+    #config generators do |g|
+    #  g.test_framework :rspec,
+    #    fixtures: true,
+    #    view_specs: false,
+    #    helper_specs: false,
+    #    routing_specs: false,
+    #    controller_specs: true,
+    #    request_specs: false
+    #  g.fixture_replacement :factory_girl, dir: "spec/factories"
+    #end
   end
 end
