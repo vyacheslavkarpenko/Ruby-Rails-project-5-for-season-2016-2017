36e973f7491e0170c3771a4c4d4bcdd321e9bf57
serjpetrenko <b.g.lupus@gmail.com>
2017-03-21 01:34:26 +0200

    Add changes to erratas inside application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RubyRailsProject5ForSeason20162017
         controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
