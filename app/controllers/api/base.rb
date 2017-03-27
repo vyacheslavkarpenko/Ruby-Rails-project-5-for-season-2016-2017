39f326b25fa014830c25663ed8513c014826d1a9
simpleway <itfortim@gmail.com>
2017-03-27 12:46:33 +0300

    Some fix.

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -7,10 +7,6 @@ module API
     format :json
 
     mount API::V1::Base
-    add_swagger_documentation(
-      api_version: "v1",
-      hide_documentation_path: true,
-      hide_format: true
-    )
+    add_swagger_documentation
   end
 end
