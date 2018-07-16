3d5cb2e0fc5e18c9a0c96d6b82cde0d9fc37968e
serjpetrenko <b.g.lupus@gmail.com>
2018-07-17 00:35:48 +0300

    Change synthax for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  has_many  :orders
+  has_secure_password
+  
   validates :phone, numericality: { greater_than: 6, allow_nil: true }
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
-  has_many  :orders
-  has_secure_password
 
   before_create :set_auth_token
-  
+
   def set_auth_token
     return if auth_token.present?
     self.auth_token = generate_auth_token
@@ -15,5 +16,4 @@ class User < ApplicationRecord
   def generate_auth_token
     SecureRandom.uuid.gsub(/\-/,'')
   end
-
 end
